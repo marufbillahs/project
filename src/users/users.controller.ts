c7eed63c7ca88d9a3afaa85d7a4e211fb5e8f249
Maruf Billah Siddiki <98986876+marufbillahs@users.noreply.github.com>
2025-04-09 21:50:03 +0600

    create user service

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
+import { UsersService } from './users.service';
+import { get } from 'http';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+  constructor(private readonly usersService: UsersService) {}
+  
+  @Get()
+  getHello(): string {
+    return this.usersService.getHello();
+  }
+}
